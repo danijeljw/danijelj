2fb4743269a19596b54d2cece7c7ebc0aeab89cf
Danijel James <danijel.james@Danijels-MacBook.local>
2016-04-06 15:51:19 +0930

    auto-update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 rake generate
 rake deploy
 git add -A -f *
-git commit -m 'auto-update'
+git commit -m 'auto-update' --amend --reset-author
 git push origin master
 rake notify
 echo "Pushed and Published!"
